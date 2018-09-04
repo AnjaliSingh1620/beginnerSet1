0a4c9c1a68ebe2d2b957260adacd74693f1ef130
AnjaliSingh1620 <42083973+AnjaliSingh1620@users.noreply.github.com>
2018-09-04 17:23:58 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -3,14 +3,11 @@ public class OddEven {
  public static void main(String args[]) {
 	 Scanner sc=new Scanner(System.in);
 	 int n=sc.nextInt();
-	 if(n%2==0) {
+	 if(n<0){
+		 System.out.println("Invalid");
+	 }
+	  if(n%2==0) {
 		 System.out.println("Even");
-	 }
-	 else if(n%2!=0) {
-		 System.out.println("Odd");
-	 }
-	 else if(n<0){
-		 System.out.println("Invalid Input");
 	 }
  }
 }
