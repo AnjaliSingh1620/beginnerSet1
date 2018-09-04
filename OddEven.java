079098cc15cf1b0bcd5ee2ecb055d7e563ac1d19
AnjaliSingh1620 <42083973+AnjaliSingh1620@users.noreply.github.com>
2018-09-04 17:17:35 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class OddEven {
+ public static void main(String args[]) {
+	 Scanner sc=new Scanner(System.in);
+	 int n=sc.nextInt();
+	 if(n%2==0) {
+		 System.out.println("Even");
+	 }
+	 else if(n%2!=0) {
+		 System.out.println("Odd");
+	 }
+	 else if(n<0){
+		 System.out.println("Invalid Input");
+	 }
+ }
+}
